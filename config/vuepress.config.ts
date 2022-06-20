f697fad973f0a3353a7f348a5187aed6b69f08fa
impossible98 <christoleavenow@outlook.com>
2022-06-20 21:08:16 +0800

    chore(devDependencies): add @vuepress/plugin-pwa

--- config/vuepress.config.ts
+++ config/vuepress.config.ts
@@ -1,8 +1,51 @@
+import { pwaPlugin } from '@vuepress/plugin-pwa';
 import { defaultTheme } from '@vuepress/theme-default';
 import { defineUserConfig } from 'vuepress';
 
 export default defineUserConfig({
   base: '/',
+  head: [
+    [
+      'link',
+      {
+        rel: 'apple-touch-icon',
+        sizes: '180x180',
+        href: '/images/pwa/apple-touch-icon.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '16x16',
+        href: '/images/pwa/favicon-16x16.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'icon',
+        type: 'image/png',
+        sizes: '32x32',
+        href: '/images/pwa/favicon-32x32.png',
+      },
+    ],
+    [
+      'link',
+      {
+        rel: 'manifest',
+        href: '/manifest.webmanifest',
+      },
+    ],
+    [
+      'meta',
+      {
+        name: 'theme-color',
+        content: '#141A25',
+      },
+    ],
+  ],
   locales: {
     '/': {
       lang: 'en-US',
@@ -44,5 +87,9 @@ export default defineUserConfig({
     },
   }),
   markdown: {},
-  plugins: [],
+  plugins: [
+    pwaPlugin({
+      skipWaiting: true,
+    }),
+  ],
 });
